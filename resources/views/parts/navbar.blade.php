fc2256d786296ed571ed615ff4904affbbf0f8d9
Frank McCoy <frankmccoy.d@gmail.com>
2018-03-10 10:29:24 -0600

    navbar-color

--- resources/views/parts/navbar.blade.php
+++ resources/views/parts/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -12,7 +12,7 @@
       <li class="nav-item">
         <a class="nav-link" href="{{ route('recipes.index') }}">Recipes</a>
       </li>
-      
+
     </ul>
 
   </div>
