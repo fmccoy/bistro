4c9c452a83e0a0cdce13b401696d18e7d3f5f7f3
Frank McCoy <frankmccoy.d@gmail.com>
2018-03-30 17:45:20 -0500

    change to table layout

--- resources/views/recipes/table.blade.php
+++ resources/views/recipes/table.blade.php
@@ -1,21 +1,31 @@
-<table class="table table-responsive">
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach ($data as $recipe)
-      <tr>
-        <td>{{ $recipe['id'] }}</td>
-        <td>{{ $recipe['name'] }}</td>
-        <td>
-          <a class="btn btn-primary" href="{{ route('recipes.show', ['id' => $recipe['id']]) }}"><span class="oi oi-eye"></span></a>
-          <a class="btn btn-primary" href="{{ route('recipes.edit', ['id' => $recipe['id']]) }}"><span class="oi oi-pencil"></span></a>
-        </td>
-      </tr>
-    @endforeach
-  </tbody>
-</table>
+<div class="col-sm-10 offset-col-1">
+  <div class="card">
+
+    <div class="card-header bg-secondary text-light">
+      <h2>Recipes</h2>
+    </div>
+
+    <table class="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($data as $recipe)
+          <tr>
+            <td>{{ $recipe['id'] }}</td>
+            <td>{{ $recipe['name'] }}</td>
+            <td>
+              <a class="btn btn-primary" href="{{ route('recipes.show', ['id' => $recipe['id']]) }}"><span class="oi oi-eye"></span></a>
+              <a class="btn btn-primary" href="{{ route('recipes.edit', ['id' => $recipe['id']]) }}"><span class="oi oi-pencil"></span></a>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+  </div>
+</div>
